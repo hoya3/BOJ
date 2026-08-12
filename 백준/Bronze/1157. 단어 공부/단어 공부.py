5a6d174ebce1faf3c745501d916200584753efc8
HOGEUN <70062818+hoya3@users.noreply.github.com>
2026-08-12 16:01:12 +0900

    Delete 백준 directory

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++
@@ -1,9 +0,0 @@
-from collections import Counter
-
-word = input().lower()
-cnt = Counter(word)
-most = cnt.most_common(2)
-if len(most) == 1 or most[0][1] != most[1][1]:
-    print(most[0][0].upper())
-else:
-    print("?")
--- 백준/Bronze/2525. 오븐 시계/오븐 시계.py
+++
@@ -1,8 +0,0 @@
-h, m = map(int, input().split())
-t = int(input())
-
-m = m + t
-h = h + m // 60
-m = m % 60
-h = h % 24
-print(h, m)
